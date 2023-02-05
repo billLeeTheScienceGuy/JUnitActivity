d17e2d85a6d28369812f8370e37cd00f69919899
billLeeTheScienceGuy <104463597+billLeeTheScienceGuy@users.noreply.github.com>
2023-02-04 18:05:11 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+    	MyList instance<Integer> = new MyList()<>;
+    	instance.add(2);
+    	instance.add(3);
+    	instance.add(4);
+    	instance.add(5);
+    	instance.add(6);
+    	instance.get(1);
+    	System.out.println(instance.get(2));
+    	
+    }
+
+}
